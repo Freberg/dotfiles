10a7792a851eff382da28bffcb5dcb7871093391
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2025-03-16 22:24:27 +0100

    added harpoon and theming to nvim

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -62,7 +62,6 @@ return {
       },
       snippet = {
         expand = function(args)
-          -- You need Neovim v0.10 to use vim.snippet
           vim.snippet.expand(args.body)
         end,
       },
