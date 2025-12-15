0f8f13f4967dba17979afb0d0cb023840cd8d865
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2025-12-15 11:31:42 +0100

    enable preview by default in oil file explorer

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,6 +4,7 @@ return {
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   lazy = false,
   config = function()
+    vim.opt.splitright = true
     require("oil").setup {
       view_options = {
         show_hidden = true,
@@ -13,7 +14,22 @@ return {
         max_height = 0.8
       }
     }
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+
+    vim.keymap.set("n", "-", function()
+      if vim.w.is_oil_win then
+        require('oil').close()
+      else
+        require('oil').open(nil, { preview = {} })
+      end
+    end, { desc = "Toggle file explorer" })
+
+    vim.keymap.set('n', '<leader>-', function()
+      if vim.w.is_oil_win then
+        require('oil').close()
+      else
+        require('oil').open_float(nil, { preview = {} })
+      end
+    end, { desc = 'Toggle float file exlorer', silent = true })
+
   end
 }
