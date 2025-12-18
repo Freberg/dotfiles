cf242348edf14c5ced837a0c0f4dd3d81479806a
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2025-12-18 11:22:04 +0100

    added niri compositor

---
+++ flake/features/niri/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  programs.niri = {
+    enable = true;
+  };
+
+  xdg = {
+    portal = {
+      enable = true; 
+
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gnome
+  ];
+}
--- flake/hosts/workstation_common.nix
+++ flake/hosts/workstation_common.nix
@@ -7,6 +7,7 @@
     ../features/audio/default.nix
     ../features/gnome/default.nix
     ../features/hyprland/default.nix
+    ../features/niri/default.nix
     ../features/kanata/default.nix
   ];
 }
