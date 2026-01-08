20ff02383abc1ade67ff761cd7e72b9019c0307a
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-01-08 21:40:59 +0100

    added opencode and ollama options to reduce vram usage

--- flake/home-manager/ai.nix
+++ flake/home-manager/ai.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ pkgsUnstable, ... }:
 {
   services.ollama = {
     enable = true;
     acceleration = "cuda";
+    environmentVariables = {
+      OLLAMA_KV_CACHE_TYPE = "q8_0";
+      OLLAMA_FLASH_ATTENTION = "1";
+    };
   };
+
+  home.packages = [
+    pkgsUnstable.opencode
+  ];
 }
