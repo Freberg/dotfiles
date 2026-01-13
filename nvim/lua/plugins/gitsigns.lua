d05662b4e7674d18ac4e4719b12dd5d1d924b683
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-01-13 21:55:19 +0100

    added keybinds for git blame in gitsigns plugin

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,5 +2,19 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require('gitsigns').setup()
-  end
+  end,
+  keys = {
+    {
+      "<leader>ba",
+      "<CMD>Gitsigns<CR>",
+      mode = { "n", "x" },
+      desc = "Open gitsigns actions",
+    },
+    {
+      "<leader>bb",
+      "<CMD>Gitsigns blame<CR>",
+      mode = { "n", "x" },
+      desc = "Open gitsigns blame",
+    },
+  }
 }
