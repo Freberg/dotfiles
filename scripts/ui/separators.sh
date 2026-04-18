c87792ac6576347f884d3490dbae89afc1ee0b6f
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-04-18 13:48:32 +0200

    added thin separators to waybar

--- scripts/ui/separators.sh
+++ scripts/ui/separators.sh
@@ -3,13 +3,9 @@
 source "$HOME/.config/theme/env.sh"
 
 case "$1" in
-    left)
-        echo "$SEPARATOR_LEFT"
-        ;;
-    right)
-        echo "$SEPARATOR_RIGHT"
-        ;;
-    *)
-        exit 1
-        ;;
+    left)       echo "$SEPARATOR_LEFT" ;;
+    right)      echo "$SEPARATOR_RIGHT" ;;
+    left-thin)  echo "$SEPARATOR_LEFT_THIN" ;;
+    right-thin) echo "$SEPARATOR_RIGHT_THIN" ;;
+    *)          exit 1 ;;
 esac
