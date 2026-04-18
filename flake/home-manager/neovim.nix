606cd52b05feb1a3e22c57ded77042bcc39e97d5
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-04-18 15:15:20 +0200

    added css lsp

--- flake/home-manager/neovim.nix
+++ flake/home-manager/neovim.nix
@@ -14,6 +14,7 @@
     pyright
     ruff
     bash-language-server
+    vscode-css-languageserver
     # neovim
     pkgsUnstable.neovim
   ];
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -9,7 +9,7 @@ return {
 
     local on_attach = function(client, bufnr)
       local opts = { buffer = bufnr }
-
+      -- navigation
       vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
       vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
       vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
@@ -17,9 +17,12 @@ return {
       vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
       vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
       vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-      vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-      vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>', opts)
-      vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      -- action
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set({ 'n', 'x' }, '<leader>F', function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
     end
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -27,6 +30,7 @@ return {
     -- A list of servers to set up with the default on_attach and capabilities.
     local servers = {
       'bashls',
+      'cssls',
       'dockerls',
       'docker_compose_language_service',
       'jdtls',
