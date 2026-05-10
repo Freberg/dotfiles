cf2eb1771942736d3ad91028e323e15ed88d80ea
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-05-10 16:49:17 +0200

    added mini plugins for moving lines/blocks, auto character pairs and surrounding

---
+++ nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-mini/mini.move',
+  version = '*',
+  config = function()
+    require('mini.move').setup({
+      mappings = {
+        left = '<C-h>',
+        right = '<C-l>',
+        down = '<C-j>',
+        up = '<C-k>',
+
+        line_left = '<C-h>',
+        line_right = '<C-l>',
+        line_down = '<C-j>',
+        line_up = '<C-k>',
+      },
+    })
+  end,
+}
