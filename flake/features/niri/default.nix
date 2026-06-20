442dee193bb33449ec54a6e1aab12d3aed0457da
Daniel Freberg <11931384+Freberg@users.noreply.github.com>
2026-06-20 13:54:44 +0200

    fixed termfilechooser regression after niri update

--- flake/features/niri/default.nix
+++ flake/features/niri/default.nix
@@ -9,13 +9,21 @@
       enable = true; 
 
       extraPortals = with pkgs; [
+        xdg-desktop-portal-termfilechooser
         xdg-desktop-portal-gtk
         xdg-desktop-portal-gnome
       ];
+
+      config = {
+        niri = {
+          "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
+        };
+      };
     };
   };
 
   environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-termfilechooser
     xdg-desktop-portal-gtk
     xdg-desktop-portal-gnome
     xwayland-satellite
